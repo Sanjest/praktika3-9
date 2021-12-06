2192fe9f43b006a798185edfb246d7bec498d083
Sanjest <basaka228@gmail.com>
2021-12-06 13:50:17 +0400

    add new line

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int readingData(){
+    int a = 101;
+    printf("Read Date Ok...\n");
+    return a;
+}
+int main(){
+    printf("Test example 11: OK\n");
+    readingData();
+    //calcData();
+    //writeData();
+    return 0;
+}
